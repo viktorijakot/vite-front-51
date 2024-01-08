9e31d72ec6fd3b4210c735b3ec0fc2b6a3b602bd
viktorija <viktorija.kotaite@gmail.com>
2024-01-08 15:04:27 +0200

    empty login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import Users from "./Components/Users";
+// import Users from "./Components/Users";
+import Login from "./Components/auth/Login";
 
 function App() {
   return (
     <div className="container">
       <h1>Hello vite</h1>
-      <Users />
+      <Login />
     </div>
   );
 }
---
+++ src/Components/auth/Login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form noValidate>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
