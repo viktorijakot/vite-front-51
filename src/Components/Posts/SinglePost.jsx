98515db18d5c81ef224e82348160e2e36b24a3f2
viktorija <viktorija.kotaite@gmail.com>
2024-01-16 13:42:55 +0200

    my post

--- src/Components/Posts/SinglePost.jsx
+++ src/Components/Posts/SinglePost.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../../store/authContext";
 const postObj = {
   post_id: 1,
   title: "Post 1",
   author: "James Band",
+  userEmail: "j@b.com",
   content: "Body of post 1",
   date: "2023-12-26T22:00:00.000Z",
   commentCount: 1,
@@ -11,8 +13,16 @@ const postObj = {
 };
 
 function SinglePost({ post }) {
+  const { userEmail } = useAuthContext();
+
   return (
-    <li className="card">
+    <li
+      className={`card ${
+        userEmail === post.userEmail
+          ? `border border-success bg-success-subtle`
+          : ``
+      }`}
+    >
       <div className="card-body">
         <h5 className="card-title">{post.title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{post.author}</h6>
